86ae3df162e4a45492f4381ae86e743720afe9a8
Amirhoseinjkar <150539608+Amirhoseinjkar@users.noreply.github.com>
2026-07-25 13:50:36 +0430

    some css changes for the movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,33 +1,38 @@
 import "./movieCard.css";
 import { Link } from "react-router-dom";
 import "../pages/movieDetails.css";
-function MovieCard({ movie}) {
+function MovieCard({ movie }) {
   return (
     <>
       {
-        <Link className="movie-link" draggable={false} key={movie.id} to={`/movie/${movie.id}`}>
-          <div className="movie-card" >
-            <img draggable='false'
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
+         <Link
+    className="movie-link"
+    draggable={false}
+    to={`/movie/${movie.id}`}
+  >
+    <div className="movie-card">
+      <img
+        draggable={false}
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
 
-            <div className="movie-info">
-              <h3 className="movie-title">{movie.title}</h3>
-              <div className="movie-year-rating">
-                <span className="movie-rating">
-                  <img src="src\assets\star.png" alt="Star" />{" "}
-                  {movie.vote_average.toFixed(1)}
-                </span>
-                <p>{movie.release_date}</p>
-              </div>
+      <div className="movie-info">
+        <h3 className="movie-title">{movie.title}</h3>
 
-              <p className="overview">
-                {movie.overview.split(" ").splice(0, 15).join(" ")} ...
-              </p>
-            </div>
-          </div>
-        </Link>
+        <div className="movie-footer">
+          <span className="movie-rating">
+            <img src="src/assets/star.png" alt="Star" />
+            {movie.vote_average.toFixed(1)}
+          </span>
+
+          <span className="movie-year">
+            {movie.release_date?.slice(0, 4)}
+          </span>
+        </div>
+      </div>
+    </div>
+  </Link>
       }
     </>
   );
